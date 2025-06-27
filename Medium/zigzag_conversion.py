46223144247f898153f21548901061df64149017
Ms-moraveji1973 <ms.moraveji2007@gmail.com>
2025-06-27 22:43:16 +0200

    solve: leetcode zigzag conversion problem

--- Medium/zigzag_conversion.py
+++ Medium/zigzag_conversion.py
@@ -1,5 +1,5 @@
 '''
-[mohammad] => [mm] , [oamd] , [ha]
+[mohammad],3 => [mm] , [oamd] , [ha]
 m = 1
 o = 2
 h = 3
@@ -10,21 +10,33 @@ a = 3
 d = 2
 '''
 
-def zigzag(array,number):
-    if number == 1:
+def zigzag(array,num_rows):
+    # If number of rows is 1, return the original text (no zigzag needed)
+    if num_rows == 1:
         return array
-    a = number*['']
-    index = 0
-    pouse = -1
+
+    # Create a list to store strings for each row
+    rows = num_rows*['']
+
+    # Current row index
+    curr_row = 0
+
+    # Direction controller:
+    # -1 means going up, 1 means going down
+    going_down = -1
+
+    # Loop through each character in the input text
     for i in array:
-        a[index] += i
-        if index == 0 or index == (number-1) :
-            pouse = -1 * pouse
-        if pouse > 0 :
-            index += 1
-        elif pouse < 0 :
-            index = index-1
-    return a
+        rows[curr_row] += i
+
+        # If we are at the top or bottom row, change direction
+        if curr_row == 0 or curr_row == (num_rows-1) :
+            going_down = -1 * going_down
+
+        # Move to the next row based on the current direction
+        going_down += 1 if going_down > 0 else -1
+        
+    return rows
 
 
-print(zigzag('adasfasasd',5))
+print(zigzag('mohammad',3))
